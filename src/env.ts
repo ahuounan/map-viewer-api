27809cf351f63d8f3da624e59bae4169ab6cdba4
Alan Hu <on.alanhu@gmail.com>
2021-08-18 18:28:53 +0100

    fix: force run of env var check

--- src/env.ts
+++ src/env.ts
@@ -4,20 +4,28 @@ import Logger from '@lib/logger';
 
 dotenv.config();
 
-const vars = ['PASSWORD', 'USERNAME', 'SECRET_KEY', 'CORS_ORIGIN', 'MAP_TOKEN'];
+export const env: Record<string, string> = (() => {
+  const vars = [
+    'PASSWORD',
+    'USERNAME',
+    'SECRET_KEY',
+    'CORS_ORIGIN',
+    'MAP_TOKEN',
+  ];
 
-for (const envVar of vars) {
-  if (!process.env[envVar]) {
-    throw new Error(`Environment variable ${envVar} not set.`);
+  for (const envVar of vars) {
+    if (!process.env[envVar]) {
+      throw new Error(`Environment variable ${envVar} not set.`);
+    }
   }
-}
 
-Logger.info('CORS_ORIGIN: ', process.env.CORS_ORIGIN);
+  Logger.info('CORS_ORIGIN: ', process.env.CORS_ORIGIN);
 
-export const env: Record<string, string> = {
-  password: process.env.PASSWORD,
-  username: process.env.USERNAME,
-  secretKey: process.env.SECRET_KEY,
-  corsOrigin: process.env.CORS_ORIGIN,
-  mapToken: process.env.MAP_TOKEN,
-} as Record<string, string>;
+  return {
+    password: process.env.PASSWORD,
+    username: process.env.USERNAME,
+    secretKey: process.env.SECRET_KEY,
+    corsOrigin: process.env.CORS_ORIGIN,
+    mapToken: process.env.MAP_TOKEN,
+  } as Record<string, string>;
+})();
